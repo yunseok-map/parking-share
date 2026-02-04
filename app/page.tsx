ca21bdb3be5a4181a96f5dd6df49a0b767e3cd2f
yunseok234 <yunseok0202@naver.com>
2026-02-04 14:32:08 +0900

    모바일 기능 추가

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,10 @@ import LoginButton from '@/components/LoginButton';
 
 export default function Home() {
   return (
-    <main>
-      <LoginButton />
+    <main className="relative">
+      <div className="absolute top-0 left-0 right-0 z-20">
+        <LoginButton />
+      </div>
       <KakaoMap />
       <BottomNav />
     </main>
