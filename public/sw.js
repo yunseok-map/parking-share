1c35a085fb7147ef8ff84f09e431fa9d1cd5d7ce
yunseok234 <yunseok0202@naver.com>
2026-02-05 14:39:15 +0900

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'parking-share-v1';
+const urlsToCache = ['/', '/list', '/add', '/my'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) return caches.delete(cacheName);
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
