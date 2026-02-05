f94834599a7e05c0e7127959cf2c234ed59cf282
yunseok234 <yunseok0202@naver.com>
2026-02-05 11:36:54 +0900

    Refactor Home component layout and structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,20 @@
+'use client';
+
 import KakaoMap from '@/components/Map';
 import BottomNav from '@/components/BottomNav';
 import LoginButton from '@/components/LoginButton';
 
 export default function Home() {
-return (
-  <div className="relative w-full h-screen">
-    <div id="map" className="w-full h-full bg-gray-100" />
-
-    {/* 로그인 버튼 (우측 상단) */}
-    <div className="absolute top-4 right-4 z-30">
-      <LoginButton />
-    </div>
+  return (
+    <div className="relative">
       <KakaoMap />
+      
+      {/* 로그인 버튼 (우측 상단) */}
+      <div className="absolute top-4 right-4 z-30">
+        <LoginButton />
+      </div>
+
       <BottomNav />
-    </main>
+    </div>
   );
 }
